0927024244dff51579e58977ec3065ca820a7ce7
Ben Field <fieldben97@gmail.com>
2021-02-17 16:22:23 +1100

    Refactored code to reduce duplicate print statements

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,66 +13,43 @@ while True:
 
     if user_move in legal_moves:
         computer_move = random.choice(legal_moves)
+        print(f"Computer plays {computer_move}")
 
         if user_move == "rock" and computer_move == "rock":
-            print(f"Computer plays {computer_move}")
             print("Draw")
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "rock" and computer_move == "paper":
-            print(f"Computer plays {computer_move}")
             print("Computer wins")
             computer_score += 1
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "rock" and computer_move == "scissors":
-            print(f"Computer plays {computer_move}")
             print("Player wins")
             player_score += 1
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "paper" and computer_move == "rock":
-            print(f"Computer plays {computer_move}")
             print("Player wins")
             player_score += 1
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "paper" and computer_move == "paper":
-            print(f"Computer plays {computer_move}")
             print("Draw")
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "paper" and computer_move == "scissors":
-            print(f"Computer plays {computer_move}")
             print("Computer wins")
             computer_score += 1
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "scissors" and computer_move == "rock":
-            print(f"Computer plays {computer_move}")
             print("Computer wins")
             computer_score += 1
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "scissors" and computer_move == "paper":
-            print(f"Computer plays {computer_move}")
             print("Player wins")
             player_score += 1
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
 
         elif user_move == "scissors" and computer_move == "scissors":
-            print(f"Computer plays {computer_move}")
             print("Draw")
-            print(f"Player score: {player_score}")
-            print(f"Compute score: {computer_score}")
+
+        print(f"Player score: {player_score}")
+        print(f"Computer score: {computer_score}")
 
     else:
         print('That is not a legal move')
